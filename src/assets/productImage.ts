904f51503e923ad58f7300e03d0dedced0270a26
unknown <jec.dev.22@gmail.com>
2023-03-30 18:20:09 -0300

    Add: Assets

---
+++ src/assets/productImage.ts
@@ -0,0 +1,33 @@
+import { ProductImage } from "src/types/product";
+import big1 from "@/image/image-product-1.jpg"
+import small1 from "@/image/image-product-1-thumbnail.jpg"
+
+import big2 from "@/image/image-product-2.jpg"
+import small2 from "@/image/image-product-2-thumbnail.jpg"
+
+import big3 from "@/image/image-product-3.jpg"
+import small3 from "@/image/image-product-3-thumbnail.jpg"
+
+import big4 from "@/image/image-product-4.jpg"
+import small4 from "@/image/image-product-4-thumbnail.jpg"
+
+const productImage: ProductImage[] = [
+  {
+    big: big1,
+    small: small1
+  },
+  {
+    big: big2,
+    small: small2
+  },
+  {
+    big: big3,
+    small: small3
+  },
+  {
+    big: big4,
+    small: small4
+  }
+]
+
+export default productImage
